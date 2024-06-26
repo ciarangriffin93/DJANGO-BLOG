47f8f061edf4108ea591beffd11beaa994b3a892
Ciaran Griffin <griffinciaran3@gmail.com>
2024-06-26 14:12:42 +0000

    readies code for deploy

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7-t2e(u1bo+70+^26z78vex-@cf7!znsyncq8s77-i&yly11b@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-ciarangriffi-djangoblog-mtoqnte1km9.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-ciarangriffi-djangoblog-dytn3k8urba.ws.codeinstitute-ide.net','.herokuapp.com']
 
 
 # Application definition
